ccca0487a9f95a34ab01e52e4383476620116581
NadiaPukhova <nadjacher2997@yandex.ru>
2023-08-25 20:42:51 +0300

    ss

--- src/index.tsx
+++ src/index.tsx
@@ -4,13 +4,12 @@ import App from './App'
 import './styles/index.scss'
 import { BrowserRouter } from 'react-router-dom'
 import { ThemeProvider } from 'provider/ThemeProvider'
+
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
-	<React.StrictMode>
-		<BrowserRouter>
-			<ThemeProvider>
-				<App />
-			</ThemeProvider>
-		</BrowserRouter>
-	</React.StrictMode>
+	<BrowserRouter>
+		<ThemeProvider>
+			<App />
+		</ThemeProvider>
+	</BrowserRouter>
 )
